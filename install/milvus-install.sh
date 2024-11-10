ea9b84b5e52300dddeed23633cb35437000141f2
Sing Chow <starstarstarchow@gmail.com>
2024-11-10 18:58:24 +0800

    milvus add attu gui

--- install/milvus-install.sh
+++ install/milvus-install.sh
@@ -18,10 +18,13 @@ msg_ok "Installed Dependencies"
 
 msg_info "Installing milvus (patience)"
 wget -q https://github.com/milvus-io/milvus/releases/download/v2.3.10/milvus_2.3.10-1_amd64.deb
+wget -q https://github.com/zilliztech/attu/releases/download/v2.4.9/attu_2.4.9_amd64.deb
 $STD apt-get update
 $STD dpkg -i milvus_2.3.10-1_amd64.deb
+$STD dpkg -i attu_2.4.9_amd64.deb
 $STD apt-get -f install
 $STD rm milvus_2.3.10-1_amd64.deb
+$STD rm attu_2.4.9_amd64.deb
 msg_ok "Installed milvus"
 
 motd_ssh

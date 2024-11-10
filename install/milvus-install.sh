6816f3cb170079d2b4c0a3d81a875c43cc714278
Sing Chow <starstarstarchow@gmail.com>
2024-11-10 18:00:48 +0800

    add milvus install

---
+++ install/milvus-install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y net-tools
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing milvus (patience)"
+wget -qO- https://github.com/milvus-io/milvus/releases/download/v2.3.10/milvus_2.3.10-1_amd64.deb
+$STD apt-get update
+$STD dpkg -i milvus_2.3.10-1_amd64.deb
+$STD apt-get -f install
+msg_ok "Installed milvus"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
